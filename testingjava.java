176a0e791875c766a48cfc42a00fae14716b4b99
Sumit Kumar <kaiizennn@Sumits-MacBook-Air.local>
2024-08-26 00:04:45 +0530

    UPTSE

---
+++ testingjava.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * testingjava
+ */
+public class Testingjava {
+    public static void main(String args[]) {
+        System.out.println("TesTING data");
+    }  
+} 
